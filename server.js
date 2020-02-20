b19ea8d4a619edf070594d4f6446e26375ed662f
Eduardo Torres Ferreira <etf1974@hotmail.com>
2020-02-20 18:26:26 -0400

    Deploy

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 
 require('dotenv').config()
-
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 express()
     .use(express.static(path.join(__dirname, '/dist')))
