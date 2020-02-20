2dc8f37f4ab586691f6292d1cddcbe1760e1e54c
Eduardo Torres Ferreira <etf1974@hotmail.com>
2020-02-20 18:03:23 -0400

    Deploy

--- server.js
+++ server.js
@@ -5,10 +5,4 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-    .use(express.static(path.join(__dirname, '/dist')))
-    //.use(serveStatic(__dirname + '/dist'))
-  //.use(express.static(path.join(__dirname, 'public')))
-  //.set('src', path.join(__dirname, 'src'))
-  //.get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
