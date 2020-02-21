c44d03362441dd88a548ffa2ebf99781ae13ed97
Eduardo Torres Ferreira <etf1974@hotmail.com>
2020-02-20 20:10:05 -0400

    Deploy

---
+++ dist/precache-manifest.7d8bd2c537afc68ec74540ebe55a2a93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8662e35f1df32e1a11",
+    "url": "/css/chunk-vendors.55699d37.css"
+  },
+  {
+    "revision": "f2a0c839a4cfee6177b5dc7d8ef720f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40d7cab1775494bf11c1",
+    "url": "/js/about.e8a5ef8f.js"
+  },
+  {
+    "revision": "8115bc0e95da44f3f33a",
+    "url": "/js/app.1efdf683.js"
+  },
+  {
+    "revision": "ea8662e35f1df32e1a11",
+    "url": "/js/chunk-vendors.e744dba5.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
