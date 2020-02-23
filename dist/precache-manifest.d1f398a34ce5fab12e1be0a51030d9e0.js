1904e053ddfb4493d7c00bc24d5219ca71bebfa0
Eduardo Torres Ferreira <etf1974@hotmail.com>
2020-02-23 17:42:10 -0400

    Deploy

---
+++ dist/precache-manifest.d1f398a34ce5fab12e1be0a51030d9e0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ecd01aad5370a41af6",
+    "url": "/css/Menu.a8c37bf4.css"
+  },
+  {
+    "revision": "2389ef85d7e95313846b",
+    "url": "/css/Menu~home.6ace4548.css"
+  },
+  {
+    "revision": "550a23ca0e9cedb719c4",
+    "url": "/css/chunk-vendors.0d604e64.css"
+  },
+  {
+    "revision": "e4eabcadec0780635cd8",
+    "url": "/css/home.75f409e4.css"
+  },
+  {
+    "revision": "29f0c2f676d3f908a2360ac4fb00742f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67ecd01aad5370a41af6",
+    "url": "/js/Menu.4033aa91.js"
+  },
+  {
+    "revision": "2389ef85d7e95313846b",
+    "url": "/js/Menu~home.fa6bb29b.js"
+  },
+  {
+    "revision": "84b78b088a6a09cac688",
+    "url": "/js/app.74a7be83.js"
+  },
+  {
+    "revision": "550a23ca0e9cedb719c4",
+    "url": "/js/chunk-vendors.2c0411bd.js"
+  },
+  {
+    "revision": "e4eabcadec0780635cd8",
+    "url": "/js/home.78fa6c22.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
