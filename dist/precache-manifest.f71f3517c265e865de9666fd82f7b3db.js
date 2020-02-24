5a0b4e996264ba97ab2a69737cd7e40621c8255a
Eduardo Torres Ferreira <etf1974@hotmail.com>
2020-02-23 20:32:29 -0400

    Deploy

---
+++ dist/precache-manifest.f71f3517c265e865de9666fd82f7b3db.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9d8e148a8b2eedb86f",
+    "url": "/css/Favoritos~Gostei~Menu.d30ccb8b.css"
+  },
+  {
+    "revision": "adef9c4a5d67ab8a938e",
+    "url": "/css/Favoritos~Gostei~Menu~Profiles~home.023713fd.css"
+  },
+  {
+    "revision": "e3e8710aa740c5acf1cd",
+    "url": "/css/Menu.2eb9f3d3.css"
+  },
+  {
+    "revision": "121686be4040b4d1a1a8",
+    "url": "/css/Menu~Profiles~home.105dd787.css"
+  },
+  {
+    "revision": "648b45518c392dade17d",
+    "url": "/css/Profiles.f613719f.css"
+  },
+  {
+    "revision": "87f3474a5a22d928c48f",
+    "url": "/css/chunk-vendors.863cd37f.css"
+  },
+  {
+    "revision": "039a241d801165700b9b",
+    "url": "/css/home.75f409e4.css"
+  },
+  {
+    "revision": "eee245b3b6cd93ffba689450253453cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e0edad24a1622f2d68e",
+    "url": "/js/Favoritos.be117831.js"
+  },
+  {
+    "revision": "ac9d8e148a8b2eedb86f",
+    "url": "/js/Favoritos~Gostei~Menu.525b2ba5.js"
+  },
+  {
+    "revision": "adef9c4a5d67ab8a938e",
+    "url": "/js/Favoritos~Gostei~Menu~Profiles~home.4586a1a6.js"
+  },
+  {
+    "revision": "ea16dfef31cc5e1966d2",
+    "url": "/js/Gostei.36bd0273.js"
+  },
+  {
+    "revision": "e3e8710aa740c5acf1cd",
+    "url": "/js/Menu.c40eff7a.js"
+  },
+  {
+    "revision": "121686be4040b4d1a1a8",
+    "url": "/js/Menu~Profiles~home.15785447.js"
+  },
+  {
+    "revision": "648b45518c392dade17d",
+    "url": "/js/Profiles.d10507c2.js"
+  },
+  {
+    "revision": "216582a2835659495169",
+    "url": "/js/app.5472e409.js"
+  },
+  {
+    "revision": "87f3474a5a22d928c48f",
+    "url": "/js/chunk-vendors.4964063e.js"
+  },
+  {
+    "revision": "039a241d801165700b9b",
+    "url": "/js/home.058a37d7.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
