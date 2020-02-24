9aed91a8edc832eb1acdfc420881887989496a77
Eduardo Torres Ferreira <etf1974@hotmail.com>
2020-02-24 17:38:52 -0400

    Deploy

---
+++ dist/precache-manifest.3841e42a6abfa58155ad4beacd4e7525.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b282c62b6a022607f0e",
+    "url": "/css/Favoritos.023713fd.css"
+  },
+  {
+    "revision": "7f1ee9f92fb48accb794",
+    "url": "/css/Favoritos~Gostei~Menu.d30ccb8b.css"
+  },
+  {
+    "revision": "1e206b357555adcf8d8d",
+    "url": "/css/Gostei.023713fd.css"
+  },
+  {
+    "revision": "0eb6fd0307f47ed07755",
+    "url": "/css/Menu.2eb9f3d3.css"
+  },
+  {
+    "revision": "45998c7361fb5899b7d4",
+    "url": "/css/Menu~Profiles~home.a7a3300d.css"
+  },
+  {
+    "revision": "648b45518c392dade17d",
+    "url": "/css/Profiles.f613719f.css"
+  },
+  {
+    "revision": "a5a5ab06a51b1db07ff8",
+    "url": "/css/chunk-vendors.fed4ef18.css"
+  },
+  {
+    "revision": "7ab12fa8129b1ffe4afb",
+    "url": "/css/home.75f409e4.css"
+  },
+  {
+    "revision": "fbc2cf6879dc1e27bee7f0d1e508f501",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b282c62b6a022607f0e",
+    "url": "/js/Favoritos.1c262235.js"
+  },
+  {
+    "revision": "7f1ee9f92fb48accb794",
+    "url": "/js/Favoritos~Gostei~Menu.97be5ce9.js"
+  },
+  {
+    "revision": "1e206b357555adcf8d8d",
+    "url": "/js/Gostei.9f2dfb19.js"
+  },
+  {
+    "revision": "0eb6fd0307f47ed07755",
+    "url": "/js/Menu.9f991ebd.js"
+  },
+  {
+    "revision": "45998c7361fb5899b7d4",
+    "url": "/js/Menu~Profiles~home.923071f4.js"
+  },
+  {
+    "revision": "648b45518c392dade17d",
+    "url": "/js/Profiles.d10507c2.js"
+  },
+  {
+    "revision": "b3594152ed5b4eeb36d4",
+    "url": "/js/app.348adc56.js"
+  },
+  {
+    "revision": "a5a5ab06a51b1db07ff8",
+    "url": "/js/chunk-vendors.abe77b15.js"
+  },
+  {
+    "revision": "7ab12fa8129b1ffe4afb",
+    "url": "/js/home.a99df1d3.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
